88eded7ce9fab3d75a300dfb57b80ea8269d1603
Célio Passos <celio.passosjr@gmail.com>
2020-08-18 12:46:49 -0300

    Add KMP

---
+++ kmp.cpp
@@ -0,0 +1,60 @@
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define debug(x) cerr << #x << " == " << (x) << '\n';
+#define all(X) X.begin(), X.end()
+#define sz(X) (int)X.size()
+
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ll = long long;
+
+mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
+
+// xz
+
+vector<int> prefix_function(string s)
+{
+    int n = sz(s);
+    vector p(n, 0);
+    for (int i = 1; i < n; ++i)
+    {
+        int j = p[i - 1];
+        while (j > 0 && s[i] != s[j]) j = p[i - 1];
+        if (s[i] == s[j]) ++j;
+        p[i] = j;
+    }
+    return p;
+}
+
+vector<vector<int>> compute_automaton(string s)
+{
+    s += '#';
+    int n = sz(s);
+    vector p = prefix_function(s);
+    vector aut(n, vector(26, 0));
+    for (int i = 0; i < n; ++i)
+        for (int c = 0; c < 26; ++c)
+        {
+            if (i > 0 && 'a' + c != s[i])
+                aut[i][c] = aut[p[i - 1]][c];
+            else
+                aut[i][c] = i + ('a' + c == s[i]);
+        }
+    return aut;
+}
+
+int main()
+{ _
+    exit(0);
+}
+
