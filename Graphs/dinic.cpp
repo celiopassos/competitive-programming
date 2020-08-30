8ccce9d10cd6828956d2f5afc14ad168c2a7084b
Célio Passos <celio.passosjr@gmail.com>
2020-08-30 15:47:45 -0300

    Shorten template

--- Graphs/dinic.cpp
+++ Graphs/dinic.cpp
@@ -1,9 +1,6 @@
 #include "bits/stdc++.h"
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/detail/standard_policies.hpp>
 
 using namespace std;
-using namespace __gnu_pbds;
 
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
@@ -11,20 +8,12 @@ using namespace __gnu_pbds;
 #define all(X) X.begin(), X.end()
 #define sz(X) (int)X.size()
 
-template<typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 using ll = long long;
-
-mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
 // xz
-
-int n;
-vvi C;
-vvi E;
 
 struct FlowEdge {
     int v, u;
