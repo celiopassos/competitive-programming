eb12b5b8ba0633fb167ca007100c1df2ff4805bc
Célio Passos <celio.passosjr@gmail.com>
2020-09-01 08:14:18 -0300

    Rename directories and add integer mod p

---
+++ Number Theory/integermodp.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define debug(x) cerr << #x << " == " << (x) << '\n';
+#define all(X) X.begin(), X.end()
+#define sz(X) (int)X.size()
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
+
+// xz
+
+using F = ll;
+
+const F MOD = 1e9 + 7;
+
+F power(F x, int p)
+{
+    F res = 1;
+    while (p)
+    {
+        if (p & 1) res = (res * x) % MOD;
+        p >>= 1;
+        x = (x * x) % MOD;
+    }
+    return res;
+}
+
+struct IntegerModp
+{
+    F x;
+    IntegerModp(F x = 0) : x(x) {};
+    IntegerModp inv() const { return F(power(x, MOD - 2)); }
+    IntegerModp& operator+=(const IntegerModp& rhs) { x = (x + rhs.x) % MOD; return *this; }
+    IntegerModp& operator-=(const IntegerModp& rhs) { x = (x + MOD - rhs.x) % MOD; return *this; }
+    IntegerModp& operator*=(const IntegerModp& rhs) { x = (x * rhs.x) % MOD; return *this; }
+    IntegerModp& operator/=(const IntegerModp& rhs) { return *this *= rhs.inv(); }
+    IntegerModp operator+(const IntegerModp& rhs) const { return IntegerModp(*this) += rhs; }
+    IntegerModp operator-(const IntegerModp& rhs) const { return IntegerModp(*this) -= rhs; }
+    IntegerModp operator*(const IntegerModp& rhs) const { return IntegerModp(*this) *= rhs; }
+    IntegerModp operator/(const IntegerModp& rhs) const { return IntegerModp(*this) /= rhs; }
+    bool operator==(const IntegerModp& rhs) const { return x == rhs.x; }
+    bool operator<(const IntegerModp& rhs) const { return x < rhs.x; }
+};
+
+int main()
+{ _
+    exit(0);
+}
+
