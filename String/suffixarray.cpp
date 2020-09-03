8ff94c5de214e6b8a06408ab525b46be3c850a76
Célio Passos <celio.passosjr@gmail.com>
2020-09-03 09:52:49 -0300

    LCP in O(1)

--- String/suffixarray.cpp
+++ String/suffixarray.cpp
@@ -17,83 +17,84 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
 typedef complex<int> cp;
 
-vector<vi> c;
-vi sort_cyclic_shifts(const string& s)
+vector<int> sort_cyclic_shifts(const string& s)
 {
     int n = sz(s);
     const int alphabet = 256;
-    vi p(n), cnt(max(alphabet, n), 0);
-    c.push_back(vi(n));
-    for (int i = 0; i < n; ++i)
-        ++cnt[s[i]];
-    for (int i = 1; i < alphabet; ++i)
-        cnt[i] += cnt[i - 1];
-    for (int i = 0; i < n; ++i)
-        p[--cnt[s[i]]] = i;
-    c[0][p[0]] = 0;
+    vector p(n, 0), c(n, 0), cnt(max(alphabet, n), 0);
+    for (int i = 0; i < n; i++) cnt[s[i]]++;
+    for (int i = 1; i < alphabet; i++) cnt[i] += cnt[i-1];
+    for (int i = 0; i < n; i++) p[--cnt[s[i]]] = i;
+    c[p[0]] = 0;
     int classes = 1;
-    for (int i = 1; i < n; ++i)
+    for (int i = 1; i < n; i++)
     {
-        if (s[p[i]] != s[p[i - 1]])
-            ++classes;
-        c[0][p[i]] = classes - 1;
+        if (s[p[i]] != s[p[i-1]]) classes++;
+        c[p[i]] = classes - 1;
     }
-    vi pn(n);
+    vector pn(n, 0), cn(n, 0);
     for (int h = 0; (1 << h) < n; ++h)
     {
-        c.push_back(vi(n));
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++)
         {
             pn[i] = p[i] - (1 << h);
-            if (pn[i] < 0)
-                pn[i] += n;
+            if (pn[i] < 0) pn[i] += n;
         }
         fill(cnt.begin(), cnt.begin() + classes, 0);
-        for (int i = 0; i < n; ++i)
-            ++cnt[c[h][pn[i]]];
-        for (int i = 1; i < classes; ++i)
-            cnt[i] += cnt[i - 1];
-        for (int i = n - 1; i >= 0; --i)
-            p[--cnt[c[h][pn[i]]]] = pn[i];
-        c[h + 1][p[0]] = 0;
+        for (int i = 0; i < n; i++) cnt[c[pn[i]]]++;
+        for (int i = 1; i < classes; i++) cnt[i] += cnt[i-1];
+        for (int i = n-1; i >= 0; i--) p[--cnt[c[pn[i]]]] = pn[i];
+        cn[p[0]] = 0;
         classes = 1;
-        for (int i = 1; i < n; ++i)
+        for (int i = 1; i < n; i++)
         {
-            cp cur = { c[h][p[i]], c[h][(p[i] + (1 << h)) % n] };
-            cp prev = { c[h][p[i - 1]], c[h][(p[i - 1] + (1 << h)) % n] };
-            if (cur != prev)
-                ++classes;
-            c[h + 1][p[i]] = classes - 1;
+            pair cur = { c[p[i]], c[(p[i] + (1 << h)) % n] };
+            pair prev = { c[p[i-1]], c[(p[i-1] + (1 << h)) % n] };
+            if (cur != prev) ++classes;
+            cn[p[i]] = classes - 1;
         }
+        c.swap(cn);
     }
     return p;
 }
 
-vi suffix_array(string s)
+vector<int> suffix_array(string s)
 {
     s += "$";
-    vi sorted_shifts = sort_cyclic_shifts(s);
+    auto sorted_shifts = sort_cyclic_shifts(s);
     sorted_shifts.erase(sorted_shifts.begin());
     return sorted_shifts;
 }
 
-int lcp(int i, int j)
+vector<int> lcp_array(const string& s, const vector<int>& p)
 {
-    int ans = 0, n = sz(c[0]);
-    for (int k = sz(c) - 1; k >= 0; k--)
+    int n = sz(s);
+    vector pos(n, 0);
+    for (int i = 0; i < n; ++i) pos[p[i]] = i;
+
+    int k = 0;
+    vector lcp(n - 1, 0);
+    for (int i = 0; i < n; ++i)
     {
-        if (max(i, j) < n && c[k][i] == c[k][j])
-        {
-            ans += 1 << k;
-            i += 1 << k;
-            j += 1 << k;
-        }
+        if (pos[i] == n - 1) { k = 0; continue; }
+        int j = p[pos[i] + 1];
+        while (i + k < n && j + k < n && s[i + k] == s[j + k]) ++k;
+        lcp[pos[i]] = k;
+        if (k) --k;
     }
-    return ans;
+    return lcp;
 }
+
 
 int main()
 { _
+    // auto lcp_query = [&](int i, int j)
+    // {
+    //     if (i == j) return n - i;
+    //     if (pos[i] > pos[j]) swap(i, j);
+    //     return sparse_table.query(pos[i], pos[j] - 1);
+    // };
+
     exit(0);
 }
 
