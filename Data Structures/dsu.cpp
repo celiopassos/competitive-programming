3fe74f49f573830d87a4da5d3fd115e200b7ba24
Célio Passos <celio.passosjr@gmail.com>
2020-09-08 10:20:33 -0300

    Add DSU with rollbacks

---
+++ Data Structures/dsu.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define debug(x) cerr << #x << " == " << (x) << '\n';
+#define all(X) X.begin(), X.end()
+#define sz(X) (int)X.size()
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
+
+// xz
+
+class DSU
+{
+private:
+    vector<int> p, rk, sz;
+    int num_disjoint_sets;
+public:
+    DSU(int n) : num_disjoint_sets(n)
+    {
+        rk.assign(n, 0), p.assign(n, 0), sz.assign(n, 1);
+        for (int i = 0; i < n; ++i) p[i] = i;
+    }
+    int find_set(int i)
+    {
+        while (p[i] != i) i = p[i];
+        return i;
+    }
+    int set_size(int i) const { return sz[find_set(i)]; }
+    int num_sets() const { return num_disjoint_sets; }
+    void union_set(int i, int j)
+    {
+        int x = find_set(i), y = find_set(j);
+        if (rk[x] < rk[y]) swap(x, y);
+        if (x != y)
+        {
+            --num_disjoint_sets;
+            p[y] = x;
+            rk[x] += (rk[x] == rk[y]);
+            sz[x] += sz[y];
+        }
+    }
+};
+
+int main()
+{ _
+    exit(0);
+}
---
+++ Data Structures/dsurollbacks.cpp
@@ -0,0 +1,71 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define debug(x) cerr << #x << " == " << (x) << '\n';
+#define all(X) X.begin(), X.end()
+#define sz(X) (int)X.size()
+
+using ll = long long;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
+
+// xz
+
+class DSU
+{
+private:
+    vector<int> p, sz;
+    stack<pair<int&, int>> snapshots;
+    int num_disjoint_sets;
+public:
+    DSU(const int n) : num_disjoint_sets(n)
+    {
+        p.assign(n, 0), sz.assign(n, 1);
+        for (int i = 0; i < n; ++i) p[i] = i;
+    }
+    int find_set(int i) const
+    {
+        while (p[i] != i) i = p[i];
+        return i;
+    }
+    int set_size(int i) const { return sz[find_set(i)]; }
+    int num_sets() const { return num_disjoint_sets; }
+    void save(int& x)
+    {
+        snapshots.push(pair<int&, int>(x, x));
+    }
+    void rollback()
+    {
+        for (int t = 0; t < 3; ++t)
+        {
+            snapshots.top().first = snapshots.top().second;
+            snapshots.pop();
+        }
+    }
+    void union_set(int i, int j)
+    {
+        int x = find_set(i), y = find_set(j);
+
+        if (sz[x] < sz[y]) swap(x, y);
+
+        save(num_disjoint_sets);
+        save(p[y]);
+        save(sz[x]);
+
+        if (x != y)
+        {
+            --num_disjoint_sets;
+            p[y] = x;
+            sz[x] += sz[y];
+        }
+    }
+};
+
+int main()
+{ _
+    exit(0);
+}
