d90a2ff46afe12ad5c3b532e59e253e9f224e15a
Célio Passos <celio.passosjr@gmail.com>
2020-09-17 09:15:40 -0300

    Shorter tarjan

--- Graphs/tarjan.cpp
+++ Graphs/tarjan.cpp
@@ -16,31 +16,24 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 auto tarjan(const auto& E)
 {
     int n = sz(E);
-    vector low(n, -1), num(n, -1), scc(n, -1);
-
-    vector active(n, 0);
+    vector low(n, -1), num(n, -1), scc(n, -1), active(n, 0);
     stack<int> st; int ct = 0;
     function<void(int)> dfs = [&](int u)
     {
         low[u] = num[u] = ct++;
         active[u] = 1; st.push(u);
-
         for (auto v : E[u])
         {
             if (num[v] == -1) dfs(v);
             if (active[v]) low[u] = min(low[u], low[v]);
         }
-
         if (low[u] == num[u]) do
         {
             scc[st.top()] = u;
             active[st.top()] = 0; st.pop();
         } while (not st.empty() && num[st.top()] >= num[u]);
     };
-
-    for (int u = 0; u < n; ++u)
-        if (num[u] == -1) dfs(u);
-
+    for (int u = 0; u < n; ++u) if (num[u] == -1) dfs(u);
     return scc;
 }
 
