e5a883a2254df7c3e94e1132b7f94b313029b180
Célio Passos <celio.passosjr@gmail.com>
2020-09-27 18:42:48 -0300

    Better LiChaoTree

--- Optimization/lichaotree.cpp
+++ Optimization/lichaotree.cpp
@@ -13,50 +13,94 @@ using ll = long long;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
-template<typename T, typename F>
+// May define any kind of function,
+// as long as intersections are unique.
+
+// E.g., linear function:
+
+template<typename T>
+struct Function
+{
+    T a = 0, b = numeric_limits<T>::max();
+    T get(T x) { return a * x + b; }
+};
+
+template<typename Func, typename Codomain, typename Domain=Codomain>
 struct LiChaoTree
 {
-    const int L, R;
-    const function<T(F, int)> get;
-    const F id;
-    vector<F> st;
+    Domain L, R;
+    Func id;
+    vector<Func> st;
     vector<int> LEFT, RIGHT;
-    LiChaoTree(int L, int R, function<T(F, int)> get, F id) :
-        L(L), R(R), get(get), id(id) { create(); }
+
+    LiChaoTree(Domain L, Domain R, Func id) : L(L), R(R), id(id) { create(); }
+
+    int size() { return sz(st); }
     int create()
     {
-        LEFT.push_back(-1);
-        RIGHT.push_back(-1);
+        LEFT.push_back(-1), RIGHT.push_back(-1);
         st.push_back(id);
-        return sz(st) - 1;
+        return size() - 1;
     }
-    int get_left(int p) { return LEFT[p] == -1 ? LEFT[p] = create() : LEFT[p]; }
-    int get_right(int p) { return RIGHT[p] == -1 ? RIGHT[p] = create() : RIGHT[p]; }
-    void update(F nw)
-    {
-        for (int p = 0, l = L, r = R; l <= r;)
-        {
-            int m = l + (r - l) / 2;
-            bool left = get(nw, l) < get(st[p], l);
-            bool mid = get(nw, m) < get(st[p], m);
+    int getleft(int p) { return LEFT[p] == -1 ? LEFT[p] = create() : LEFT[p]; }
+    int getright(int p) { return RIGHT[p] == -1 ? RIGHT[p] = create() : RIGHT[p]; }
 
-            if (mid) swap(nw, st[p]);
-            if (left != mid) p = get_left(p), r = m;
-            else p = get_right(p), l = m + 1;
+    void update(Func add)
+    {
+        Domain l = L, r = R;
+        int p = 0;
+        while (l <= r)
+        {
+            Domain m = l + (r - l) / 2;
+
+            bool left = add.get(l) < st[p].get(l);
+            bool mid = add.get(m) < st[p].get(m);
+
+            if (mid) swap(add, st[p]);
+
+            if (left != mid)
+            {
+                p = getleft(p);
+                r = m;
+            }
+            else
+            {
+                p = getright(p);
+                l = m + 1;
+            }
         }
     }
-    T compute(int x)
+    Codomain compute(Domain x) // returns minimum
     {
-        T res = T(LINF);
-        for (int p = 0, l = L, r = R; l <= r;)
+        Codomain res = numeric_limits<Codomain>::max();
+        Domain l = L, r = R;
+        int p = 0;
+        while (l <= r)
         {
-            res = min(res, get(st[p], x));
-            int m = l + (r - l) / 2;
+            res = min(res, st[p].get(x));
+            Domain m = l + (r - l) / 2;
             if (x == m) break;
-            else if (x < m) r = m, p = get_left(p);
-            else l = m + 1, p = get_right(p);
+            else if (x < m)
+            {
+                p = getleft(p);
+                r = m;
+            }
+            else
+            {
+                p = getright(p);
+                l = m + 1;
+            }
         }
         return res;
+    }
+    friend void swap(LiChaoTree& lhs, LiChaoTree& rhs)
+    {
+        std::swap(lhs.L, rhs.L);
+        std::swap(lhs.R, rhs.R);
+        std::swap(lhs.id, rhs.id);
+        std::swap(lhs.st, rhs.st);
+        std::swap(lhs.LEFT, rhs.LEFT);
+        std::swap(lhs.RIGHT, rhs.RIGHT);
     }
 };
 
