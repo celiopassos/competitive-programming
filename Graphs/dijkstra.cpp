f2116f529b1e064b79905e9dd52d9908e339384f
Célio Passos <celio.passosjr@gmail.com>
2020-10-04 15:58:25 -0300

    Better dijkstra

--- Graphs/dijkstra.cpp
+++ Graphs/dijkstra.cpp
@@ -13,6 +13,11 @@ using ll = long long;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
+bool chmin(auto& x, auto y)
+{
+    return y < x ? x = y, true : false;
+}
+
 int main()
 { _
     int n, m; cin >> n >> m;
@@ -20,21 +25,24 @@ int main()
     for (int j = 0; j < m; ++j)
     {
         int u, v, w; cin >> u >> v >> w; --u, --v;
-        E[u].push_back(pair(v, w)), E[v].push_back(pair(u, w));
+        E[u].emplace_back(v, w), E[v].emplace_back(u, w);
     }
 
-    auto chmin = [&](int& x, int y) { return y < x ? x = y, true : false; };
     auto dijkstra = [&](int s)
     {
         vector dist(n, INF); dist[s] = 0;
-        priority_queue<ii, vector<ii>, greater<ii>> pq; pq.push(pair(0, s));
+        struct Data
+        {
+            int key; int v;
+            bool operator<(const Data& rhs) const { return key > rhs.key; }
+        };
+        priority_queue<Data> pq; pq.push(Data{ 0, s });
         while (!pq.empty())
         {
             auto [d, u] = pq.top(); pq.pop();
             if (d > dist[u]) continue;
             for (auto [v, w] : E[u])
-                if (chmin(dist[v], dist[u] + w))
-                    pq.push(pair(dist[v], v));
+                if (chmin(dist[v], dist[u] + w)) pq.push(Data{ dist[v], v });
         }
         return dist;
     };
