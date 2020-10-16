cce8b5ad5fe4e5c60d20f458f2afb06b470eb5ba
Célio Passos <celio.passosjr@gmail.com>
2020-10-16 16:14:32 -0300

    Fix LCA

--- Graphs/lca.cpp
+++ Graphs/lca.cpp
@@ -12,46 +12,45 @@ using ll = long long;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
+
+struct LCA
 {
 private:
     const vector<vector<int>>& E;
-    const int n, lg;
+    const int n, logn;
     vector<int> L, R, h;
     vector<vector<int>> up;
+    int ct = 0;
     void dfs(int u, int p)
     {
         h[u] = h[p] + 1, up[u][0] = p;
-        for (int i = 1; i < lg; ++i)
-            up[u][i] = up[up[u][i-1]][i-1];
-        static int ct = 0;
+        for (int i = 1; i <= logn; ++i) up[u][i] = up[up[u][i-1]][i-1];
         L[u] = ct;
-        for (int v : E[u])
-            if (v != p) dfs(v, u);
+        for (int v : E[u]) if (v != p) dfs(v, u);
         R[u] = ct++;
     }
 public:
-    LCA(const vector<vector<int>>& E, int root) : E(E), n(size(E)), lg(32 - __builtin_clz(n))
+    LCA(const auto& E, int root) : E(E), n(size(E)), logn(31 - __builtin_clz(n))
     {
         L.assign(n, 0), R.assign(n, 0), h.assign(n, -1);
-        up.assign(n, vector<int>(lg));
+        up.assign(n, vector<int>(logn + 1, -1));
         dfs(root, root);
     }
     bool is_ancestor(int u, int v) const
     {
         return L[u] <= L[v] && R[v] <= R[u];
     }
-    int lca(int u, int v) const
+    int operator()(int u, int v) const
     {
         if (is_ancestor(u, v)) return u;
         if (is_ancestor(v, u)) return v;
-        for (int i = lg - 1; i >= 0; --i)
-            if (!is_ancestor(up[u][i], v))
-                u = up[u][i];
+        for (int i = logn; i >= 0; --i)
+            if (!is_ancestor(up[u][i], v)) u = up[u][i];
         return up[u][0];
     }
     int distance(int u, int v) const
     {
-        int w = lca(u, v);
+        int w = (*this)(u, v);
         return h[u] + h[v] - 2 * h[w];
     }
 };
