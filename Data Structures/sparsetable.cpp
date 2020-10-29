4e3be403d42e119b2bab17da6685e12aef6d2a8a
Célio Passos <celio.passosjr@gmail.com>
2020-10-29 10:04:44 -0300

    Better SparseTable

--- Data Structures/sparsetable.cpp
+++ Data Structures/sparsetable.cpp
@@ -13,26 +13,26 @@ using ll = long long;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
-template<typename T>
+template<typename T, int K>
 class SparseTable
 {
 private:
     const int n;
     const function<T(T, T)> op;
+    vector<array<T, K + 1>> st;
     vector<int> log;
-    vector<vector<T>> st;
 public:
-    SparseTable(const vector<T>& a, const function<T(T, T)> op) : n(size(a)), op(op)
+    SparseTable(const vector<T>& a, auto op) : n(size(a)), op(op), st(n), log(n + 1)
     {
-        log.assign(n + 1, 0);
         for (int x = 1; x <= n; ++x) log[x] = 31 - __builtin_clz(x);
-        st.assign(n, vector<T>(log[n] + 1, T()));
+        assert(log[n] <= K);
         for (int i = 0; i < n; ++i) st[i][0] = a[i];
         for (int x = 1; x <= log[n]; ++x)
             for (int i = 0; i + (1 << x) <= n; ++i)
                 st[i][x] = op(st[i][x - 1], st[i + (1 << (x - 1))][x - 1]);
     }
-    T query(int l, int r)
+    SparseTable(const vector<T>& a) : SparseTable(a, [](T x, T y){ return min(x, y); }) { }
+    T query(int l, int r) const
     {
         int x = log[r - l + 1];
         return op(st[l][x], st[r - (1 << x) + 1][x]);
