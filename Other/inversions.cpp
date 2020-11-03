cbe5ff259e1f382f0715e6f7b5bfb6585ab7fad8
Célio Passos <celio.passosjr@gmail.com>
2020-11-03 16:49:45 -0300

    Use static auxiliary vector in inversion counting

--- Other/inversions.cpp
+++ Other/inversions.cpp
@@ -14,30 +14,31 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
 template<typename T>
-ll merge_sort(vector<T>& v, vector<T>& u, int l, int r)
+ll merge_sort(vector<T>& v, vector<T>& aux, int l, int r)
 {
     if (r <= l) return 0LL;
     int m = l + (r - l) / 2;
-    ll res = merge_sort(v, u, l, m) + merge_sort(v, u, m + 1, r);
+    ll res = merge_sort(v, aux, l, m) + merge_sort(v, aux, m + 1, r);
     int x = l, y = m + 1, z = l;
     while (x <= m && y <= r)
     {
         if (v[x] <= v[y])
-            u[z++] = v[x++];
+            aux[z++] = v[x++];
         else
-            u[z++] = v[y++], res += m - x + 1;
+            aux[z++] = v[y++], res += m - x + 1;
     }
-    while (x <= m) u[z++] = v[x++];
-    while (y <= r) u[z++] = v[y++];
-    while (l <= r) v[--z] = u[r--];
+    while (x <= m) aux[z++] = v[x++];
+    while (y <= r) aux[z++] = v[y++];
+    while (l <= r) v[--z] = aux[r--];
     return res;
 }
 
 template<typename T>
 ll inversions(vector<T> v)
 {
-    vector<T> u = v;
-    return merge_sort(v, u, 0, size(v) - 1);
+    static vector<T> aux;
+    aux.resize(max(size(aux), size(v)));
+    return merge_sort(v, aux, 0, size(v) - 1);
 }
 
 int main()
