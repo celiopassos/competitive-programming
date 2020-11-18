f9a540ef1fb61122cf6fd33de822fa79cc5a2591
Célio Passos <celio.passosjr@gmail.com>
2020-11-18 20:22:16 -0300

    Add lower_bound to LazyST

--- Data Structures/persistentst.cpp
+++ Data Structures/persistentst.cpp
@@ -30,15 +30,20 @@ private:
     vector<int> left, right, root, last;
     int create(int rt)
     {
-        left.push_back(-1), right.push_back(-1), last.push_back(rt), st.push_back(M::id);
+        left.push_back(-1), right.push_back(-1);
+        last.push_back(rt), st.push_back(M::id);
         return size(st) - 1;
     }
     int copy(int p, int rt)
     {
         if (p != -1 && rt == last[p]) return p;
+
         int q = create(rt);
+
         if (p == -1) return q;
+
         st[q] = st[p], left[q] = left[p], right[q] = right[p];
+
         return q;
     }
     void modify(int p, int l, int r, int pos, T value)
@@ -47,18 +52,28 @@ private:
         else
         {
             int m = l + (r - l) / 2;
+
             if (pos <= m) modify(left[p] = copy(left[p], last[p]), l, m, pos, value);
+
             else modify(right[p] = copy(right[p], last[p]), m + 1, r, pos, value);
-            st[p] = M::op(left[p] != -1 ? st[left[p]] : M::id, right[p] != -1 ? st[right[p]] : M::id);
+
+            st[p] = M::op(
+                left[p] != -1 ? st[left[p]] : M::id,
+                right[p] != -1 ? st[right[p]] : M::id
+            );
         }
     }
     T query(int p, int l, int r, int ql, int qr)
     {
         if (p == -1 || r < ql || qr < l) return M::id;
+
         if (ql <= l && r <= qr) return st[p];
+
         int m = l + (r - l) / 2;
+
         T resl = query(left[p], l, m, ql, qr);
         T resr = query(right[p], m + 1, r, ql, qr);
+
         return M::op(resl, resr);
     }
     void build(int p, int l, int r, const vector<T>& a)
@@ -67,14 +82,20 @@ private:
         else
         {
             int m = l + (r - l) / 2;
+
             left[p] = create(0), right[p] = create(0);
+
             build(left[p], l, m, a), build(right[p], m + 1, r, a);
+
             st[p] = M::op(st[left[p]], st[right[p]]);
         }
     }
 public:
-    PersistentST(int n) : n(n) { root.push_back(create(0)); }
-    PersistentST(const vector<T>& a) : PersistentST(size(a)) { build(0, 0, n - 1, a); }
+    PersistentST(int n) : n(n), root(1, create(0)) { }
+    PersistentST(const vector<T>& a) : PersistentST(size(a))
+    {
+        build(0, 0, n - 1, a);
+    }
     int duplicate(int version)
     {
         root.push_back(copy(root[version], size(root)));
--- Data Structures/sparsest.cpp
+++ Data Structures/sparsest.cpp
@@ -43,52 +43,77 @@ private:
     using T = typename remove_const<decltype(F::Tid)>::type;
     vector<T> st; vector<F> lazy;
     vector<int> LEFT, RIGHT;
+
     int create()
     {
         st.push_back(F::Tid), lazy.push_back(F::Fid), LEFT.push_back(-1), RIGHT.push_back(-1);
         return size(st) - 1;
     }
+
     int left(int p) { return LEFT[p] == -1 ? (LEFT[p] = create()) : LEFT[p]; }
     int right(int p) { return RIGHT[p] == -1 ? (RIGHT[p] = create()) : RIGHT[p]; }
+
     void push(int p, int l, int r)
     {
         if (lazy[p] == F::Fid) return; // may wanna remove this...
+
         lazy[p].apply(st[p], l, r);
+
         if (l != r)
         {
             int m = l + (r - l) / 2;
+
             lazy[left(p)].compose(lazy[p], l, m);
             lazy[right(p)].compose(lazy[p], m + 1, r);
         }
+
         lazy[p] = F::Fid;
     }
     void update(int p, int l, int r, int ql, int qr, F op)
     {
         if (r < ql || qr < l) push(p, l, r);
-        else if (ql <= l && r <= qr) { lazy[p].compose(op, l, r); push(p, l, r); }
+        else if (ql <= l && r <= qr)
+        {
+            lazy[p].compose(op, l, r);
+            push(p, l, r);
+        }
         else
         {
             int m = l + (r - l) / 2;
+
             push(p, l, r);
+
             update(left(p), l, m, ql, qr, op);
             update(right(p), m + 1, r, ql, qr, op);
+
             st[p] = F::combine(st[left(p)], st[right(p)]);
         }
     }
     T query(int p, int l, int r, int ql, int qr)
     {
         if (p == -1 || r < ql || qr < l) return F::Tid;
+
         push(p, l, r);
+
         if (ql <= l && r <= qr) return st[p];
+
         int m = l + (r - l) / 2;
+
         T resl = query(LEFT[p], l, m, ql, qr);
         T resr = query(RIGHT[p], m + 1, r, ql, qr);
+
         return F::combine(resl, resr);
     }
 public:
     SparseST(int L, int R) : L(L), R(R) { create(); }
-    void update(int l, int r) { update(0, L, R, l, r); }
-    T query(int l, int r) { return query(0, L, R, l, r); }
+    void update(int l, int r)
+    {
+        update(0, L, R, l, r);
+    }
+    T query(int l, int r)
+    {
+        return query(0, L, R, l, r);
+    }
 };
 
 int main()
