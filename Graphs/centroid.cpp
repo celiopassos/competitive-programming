ef527d0cab593283f50848bb12ca3727720ca1b7
Célio Passos <celio.passosjr@gmail.com>
2020-11-24 08:57:49 -0300

    Readable centroid

--- Graphs/centroid.cpp
+++ Graphs/centroid.cpp
@@ -13,56 +13,76 @@ using ll = long long;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
+template<int K>
 class CentroidDecomposition
 {
 private:
+    const int n;
     const vector<vector<int>>& E;
-    vector<int> par, subsz, level, vis;
-    vector<vector<int>> dist;
+
+    vector<int> parent, weight, vis, level;
+    vector<array<int, K>> dist;
+
     int dfs(int u, int p, int h)
     {
         if (h != -1) dist[u][h] = dist[p][h] + 1;
-        subsz[u] = 1;
-        for (auto v : E[u])
-            if (not vis[v] && v != p) subsz[u] += dfs(v, u, h);
-        return subsz[u];
+
+        weight[u] = 1;
+
+        for (auto v : E[u]) if (not vis[v] && v != p)
+            weight[u] += dfs(v, u, h);
+
+        return weight[u];
     }
-    int find_centroid(int u, int p, int sz)
+    int find_centroid(int u, int p, int cut)
     {
         for (auto v : E[u])
-            if (not vis[v] && v != p && subsz[v] > sz / 2)
-                return find_centroid(v, u, sz);
+            if (not vis[v] && v != p && weight[v] > cut)
+                return find_centroid(v, u, cut);
+
         return u;
     }
     void build(int u, int p)
     {
-        int sz = dfs(u, p, p == -1 ? -1 : level[p]);
-        int centroid = find_centroid(u, p, sz);
-        par[centroid] = p, vis[centroid] = 1;
+        int total = dfs(u, p, p == -1 ? -1 : level[p]);
+        int centroid = find_centroid(u, p, total / 2);
+
         if (p != -1) level[centroid] = level[p] + 1;
-        for (auto v : E[centroid])
-            if (not vis[v]) build(v, centroid);
+
+        parent[centroid] = p, vis[centroid] = 1;
+        dist[centroid][level[centroid]] = 0;
+
+        for (auto v : E[centroid]) if (not vis[v])
+            build(v, centroid);
     }
 public:
-    CentroidDecomposition(const vector<vector<int>>& E) : E(E)
+    CentroidDecomposition(const vector<vector<int>>& E) :
+        n(size(E)), E(E), parent(n, -1), weight(n, 0), vis(n, 0), level(n, 0), dist(n)
     {
-        int n = size(E);
-        par.assign(n, -1), subsz.assign(n, 0), vis.assign(n, 0);
-        level.assign(n, 0), dist.assign(n, vector(32 - __builtin_clz(n), 0));
         build(0, -1);
     }
-    int operator[](int u) const { return par[u]; }
-    int getlevel(int u) const { return level[u]; }
+    int operator[](int u) const
+    {
+        return parent[u];
+    }
+    int getlevel(int u) const
+    {
+        return level[u];
+    }
     int lca(int u, int v) const // centroid lca, not tree lca
     {
         if (level[u] < level[v]) swap(u, v);
-        while (level[u] > level[v]) u = par[u];
-        while (u != v) u = par[u], v = par[v];
+
+        while (level[u] > level[v]) u = parent[u];
+
+        while (u != v) u = parent[u], v = parent[v];
+
         return u;
     }
     int distance(int u, int v) const
     {
         int w = lca(u, v);
+
         return dist[u][level[w]] + dist[v][level[w]];
     }
 };
