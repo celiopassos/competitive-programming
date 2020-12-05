2dc1bf37784d3e1e24312eca0dc7ceb6a5d1200d
Célio Passos <celio.passosjr@gmail.com>
2020-12-05 12:01:42 -0300

    Better geometry primitives

--- Geometry/basic.cpp
+++ Geometry/basic.cpp
@@ -13,149 +13,224 @@ using ll = long long;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
-const double PI = acos(-1);
-const double EPS = 1e-9;
+// need to test everything...
 
-template<typename T>
-bool equal(T a, T b)
-{
-    return abs(a - b) <= EPS;
-}
+using T = double;
 
-template<typename T>
-struct point
+const long double EPS = 1e-6;
+
+bool Tcmp(const T& lhs, const T& rhs) { return lhs + EPS < rhs; }
+bool Tequal(const T& lhs, const T& rhs) { return abs(lhs - rhs) <= EPS; }
+
+struct Point
 {
     T x, y;
-    point() : x(0), y(0) {}
-    point(T x, T y) : x(x), y(y) {}
-    point& operator+=(const point& rhs) { x += rhs.x, y += rhs.y; return *this; }
-    point& operator-=(const point& rhs) { x -= rhs.x, y -= rhs.y; return *this; }
-    point& operator*=(const T& rhs) { x *= rhs, y *= rhs; return *this; }
-    point& operator/=(const T& rhs) { x /= rhs, y /= rhs; return *this; }
-    point operator+(const point& rhs) const { return point(*this) += rhs; }
-    point operator-(const point& rhs) const { return point(*this) -= rhs; }
-    point operator*(const T& rhs) const { return point(*this) *= rhs; }
-    point operator/(const T& rhs) const { return point(*this) /= rhs; }
-    bool operator==(const point& rhs) const { return equal(x, rhs.x) && equal(y, rhs.y); }
-    bool operator<(const point& rhs) const { return pair(x, y) < pair(rhs.x, rhs.y); }
+    Point(T x = 0, T y = 0) : x(x), y(y) { }
+    Point operator+=(const Point& rhs) { x += rhs.x, y += rhs.y; return *this; }
+    Point operator-=(const Point& rhs) { x -= rhs.x, y -= rhs.y; return *this; }
+    Point operator+(const Point& rhs) const { return Point(*this) += rhs; }
+    Point operator-(const Point& rhs) const { return Point(*this) -= rhs; }
+    friend Point operator*(const T& alpha, const Point& pt)
+    {
+        return Point(alpha * pt.x, alpha * pt.y);
+    }
+    friend T operator*(const Point& lhs, const Point& rhs)
+    {
+        return lhs.x * rhs.x + lhs.y * rhs.y;
+    }
+    bool operator<(const Point& rhs) const
+    {
+        return pair(x, y) < pair(rhs.x, rhs.y);
+    }
+    bool operator==(const Point& rhs) const
+    {
+        return Tequal(x, rhs.x) && Tequal(y, rhs.y);
+    }
 };
 
-template<typename T>
-point<T> operator*(const T& a, const point<T>& p) { return p * a; }
+auto sq(auto x) { return x * x; }
 
-template<typename T>
-T dot(const point<T>& p, const point<T>& q) { return p.x * q.x + p.y * q.y; }
+T sqnorm(const Point& pt) { return pt * pt; }
+T norm(const Point& pt) { return sqrt(sqnorm(pt)); }
 
-template<typename T>
-T sqnorm(const point<T>& p) { return dot(p, p); }
+T sqdist(const Point& p, const Point& q) { return sqnorm(p - q); }
+T dist(const Point& p, const Point& q) { return sqrt(sqdist(p, q)); }
 
-template<typename T>
-T norm(const point<T>& p) { return sqrt(dot(p, p)); }
+int sgn(T x) { return Tequal(x, T(0)) ? 0 : Tcmp(x, T(0)) ? -1 : +1; }
 
-template<typename T>
-T proj(const point<T>& p, const point<T>& q) { return dot(p, q) / norm(q); }
+Point unit(const Point& pt) { return (T(1) / norm(pt)) * pt; }
 
-template<typename T>
-T angle(const point<T>& p, const point<T>& q) { return acos(dot(p / norm(p), q / norm(q))); }
+// counter-clockwise
 
-template<typename T>
-T det(const point<T>& p, const point<T>& q) { return p.x * q.y - p.y * q.x; }
-
-template<typename T>
-bool parallel(const point<T>& p, const point<T>& q)
+Point rotate(const Point& pt, double theta)
 {
-    return equal(det(p, q), 0);
+    double c = cos(theta), s = sin(theta);
+    return Point(c * pt.x - s * pt.y, s * pt.x + c * pt.y);
 }
 
-template<typename T>
-point<T> intersection(const point<T>& p, const point<T>& dp, const point<T>& q, const point<T>& dq)
+Point rotate90(const Point& pt) { return Point(-pt.y, pt.x); }
+
+// determinant of
+// [ p.x p.y ]
+// [ q.x q.y ]
+
+T det(const Point& p, const Point& q)
 {
-    return p + det(q - p, dq) / det(dp, dq) * dp;
+    return p.x * q.y - q.x * p.y;
 }
 
-template<typename T>
-point<T> rotate(const point<T>& p, T theta) // counter-clockwise
+struct Line
 {
-    return point<T>(cos(theta) * p.x - sin(theta) * p.y, sin(theta) * p.x + cos(theta) * p.y);
-}
-
-template<typename T>
-point<T> rotate(const point<T>& p) { return point<T>(-p.y, p.x); } // pi/2 rad counter-clockwise
-
-template<typename T>
-bool cw(const point<T>& p, const point<T>& q, const point<T>& r)
-{
-    return det(q - p, r - q) < 0;
-}
-
-template<typename T>
-bool ccw(const point<T>& p, const point<T>& q, const point<T>& r)
-{
-    return det(q - p, r - q) > 0;
-}
-
-template<typename T>
-vector<point<T>> convex_hull(vector<point<T>> a) // counter-clockwise
-{
-    sort(all(a)); a.erase(unique(all(a)), a.end());
-    point<T> p = a[0], q = a.back();
-    vector<point<T>> up = { p }, down = { p };
-    for (int i = 1; i < size(a); ++i)
+    Point p, q;
+    Line(Point p = Point(), Point q = Point()) : p(p), q(q) { }
+    bool contains(Point pt) const
     {
-        if (i == size(a) - 1 || cw(p, a[i], q))
+        return Tequal(det(pt - p, q), T(0));
+    }
+    bool operator==(const Line& rhs) const
+    {
+        return contains(rhs.p) && contains(rhs.q);
+    }
+    bool operator!=(const Line& rhs) const { return not (*this == rhs); }
+    T dist(Point pt) const
+    {
+        Point x = pt - p, y = q - p;
+
+        Point proj = (x * y / sqnorm(y)) * y;
+
+        return ::dist(x, proj);
+    }
+};
+
+pair<bool, Point> intersection(const Line& U, const Line& V)
+{
+    if (U != V && Tequal(det(U.p - U.q, V.p - V.q), T(0)))
+        return pair(false, Point());
+
+    if (U == V) return pair(true, U.p);
+
+    T alpha = det(U.p - V.p, V.p - V.q) / det(U.p - U.q, V.p - V.q);
+
+    return pair(true, U.p + alpha * (U.q - U.p));
+}
+
+struct Segment
+{
+    Line L;
+    Segment(Point p = Point(), Point q = Point()) : L(p, q) { }
+    bool operator==(const Segment& rhs) const
+    {
+        return (L.p == rhs.L.p && L.q == rhs.L.q) || (L.q == rhs.L.p && L.p == rhs.L.q);
+    }
+    bool contains(Point pt) const
+    {
+        if (not L.contains(pt)) return false;
+
+        Point x = pt - L.p, y = L.q - L.p;
+
+        T alpha = T(1) / sqnorm(y) * x * y;
+
+        return Tcmp(alpha, T(1 + EPS));
+    }
+    T dist(Point pt) const
+    {
+        Point x = pt - L.p, y = L.q - L.p;
+
+        Point proj = L.p + (x * y / sqnorm(y)) * y;
+
+        if (contains(proj))
+            return ::dist(proj, pt);
+        else
+            return min(::dist(pt, L.p), ::dist(pt, L.q));
+    }
+};
+
+pair<bool, Point> intersection(const Segment& U, const Segment& V)
+{
+    if (auto res = intersection(U.L, V.L); not res.first) return res;
+
+    if (U.L == V.L)
+    {
+        for (auto pt : { U.L.p, U.L.q }) if (V.contains(pt)) return pair(true, pt);
+        for (auto pt : { V.L.p, V.L.q }) if (U.contains(pt)) return pair(true, pt);
+        return pair(false, Point());
+    }
+    else
+    {
+        Point pt = intersection(U.L, V.L).second;
+        return pair(U.contains(pt) && V.contains(pt), pt);
+    }
+}
+
+// check if p-q-r bends clockwise on q
+
+bool cw(const Point& p, const Point& q, const Point& r)
+{
+    return Tcmp(det(q - p, r - q), T(0));
+}
+
+// check if p-q-r bends counter-clockwise on q
+
+bool ccw(const Point& p, const Point& q, const Point& r)
+{
+    return Tcmp(T(0), det(q - p, r - q));
+}
+
+vector<Point> convex_hull(vector<Point> pts)
+{
+    sort(all(pts)), pts.erase(unique(all(pts)), end(pts));
+
+    const int n = size(pts);
+
+    Point p = pts[0], q = pts[n - 1];
+
+    vector<Point> up = { p }, down = { p };
+
+    for (int i = 1; i < n; ++i)
+    {
+        if (i == n - 1 || cw(p, pts[i], q))
         {
-            while (size(up) > 1 && !cw(up[size(up) - 2], up[size(up) - 1], a[i]))
+            while (size(up) > 1 && not cw(up[size(up) - 2], up[size(up) - 1], pts[i]))
                 up.pop_back();
-            up.push_back(a[i]);
+            up.push_back(pts[i]);
         }
-        if (i == size(a) - 1 || ccw(p, a[i], q))
+        if (i == n - 1 || ccw(p, pts[i], q))
         {
-            while (size(down) >= 2 && !ccw(down[size(down) - 2], down[size(down) - 1], a[i]))
+            while (size(down) > 1 && not ccw(down[size(down) - 2], down[size(down) - 1], pts[i]))
                 down.pop_back();
-            down.push_back(a[i]);
+            down.push_back(pts[i]);
         }
     }
-    up.pop_back();
-    while (size(up) > 1) down.push_back(up.back()), up.pop_back();
+
+    up.pop_back(), reverse(all(up)), down.insert(end(down), all(up));
+
     return down;
 }
 
-template<typename T>
-struct line
+bool is_inside(const vector<Point>& hull, Point pt)
 {
-    T a, b, c;
-    line() : a(0), b(0), c(0) {}
-    line(const T& a, const T& b, const T& c) : a(a), b(b), c(c) {}
-    line(const point<T>& p, const point<T>& q)
-    {
-        a = p.y - q.y;
-        b = q.x - p.x;
-        c = - a * p.x - b * p.y;
-    }
-};
+    const int n = size(hull);
 
-template<typename T>
-bool parallel(line<T> U, line<T> V)
-{
-    return equal(det({ U.a, U.b }, { V.a, V.b }), 0);
-}
+    if (n == 0) return false;
+    if (n == 1) return pt == hull[0];
+    if (n == 2) return Segment(hull[0], hull[1]).contains(pt);
 
-template<typename T>
-point<T> intersection(line<T> U, line<T> V)
-{
-    T x = -det({ U.c, U.b }, { V.c, V.b });
-    T y = -det({ U.a, U.c }, { V.a, V.c });
-    T z = det({ U.a, U.b }, { V.a, V.b });
-    return point<T>(x / z, y / z);
-}
+    if (ccw(hull[0], hull[n - 1], pt)) return false;
+    if (cw(hull[0], hull[1], pt)) return false;
 
-template<typename T>
-bool equivalent(line<T> U, line<T> V)
-{
+    int split = 1;
+
+    // split = largest i with cw(hull[0], hull[i], pt) == false;
+
+    for (int z = n; z > 0; z >>= 1)
+        while (split + z < n && not cw(hull[0], hull[split + z], pt)) split += z;
+
+    if (split == n - 1) return true;
+
     return
-        equal(det({ U.a, U.b }, { V.a, V.b }), 0) &&
-        equal(det({ U.a, U.c }, { V.a, V.c }), 0) &&
-        equal(det({ U.b, U.c }, { V.b, V.c }), 0);
+        (not cw(hull[0], hull[split], pt)) &&
+        (not cw(hull[split], hull[split + 1], pt)) &&
+        (not cw(hull[split + 1], hull[0], pt));
 }
 
 int main()
