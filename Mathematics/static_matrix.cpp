f6bb79e99470dd2021ba5d4d1b57721c41ae66b0
Célio Passos <celio.passosjr@gmail.com>
2020-12-06 17:58:32 -0300

    Remove stuff

--- Mathematics/static_matrix.cpp
+++ Mathematics/static_matrix.cpp
@@ -13,18 +13,6 @@ using ll = long long;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
-ll modpow(ll x, ll p, ll mod)
-{
-    x %= mod, (p %= mod - 1) < 0 ? p += mod - 1 : 0LL;
-    ll res = 1LL;
-    while (p > 0)
-    {
-        if (p & 1) res = res * x % mod;
-        x = x * x % mod, p >>= 1;
-    }
-    return res;
-}
-
 template<typename T>
 T templatepow(T x, ll p)
 {
@@ -37,40 +25,6 @@ T templatepow(T x, ll p)
     }
     return res;
 }
-
-template<int mod, bool safe = false>
-struct Mint
-{
-    int x;
-    Mint(ll x = 0) : x(int((x %= mod) < 0 ? x + mod : x)) {};
-    Mint inv() const
-    {
-        if (safe) assert(x != 0);
-        return Mint(modpow(x, mod - 2, mod));
-    }
-    Mint& operator+=(const Mint& rhs) { if ((x += rhs.x) >= mod) x -= mod; return *this; }
-    Mint& operator-=(const Mint& rhs) { return *this += mod - rhs.x; }
-    Mint& operator*=(const Mint& rhs) { x = int((1LL * x * rhs.x) % mod); return *this; }
-    Mint& operator/=(const Mint& rhs) { return *this *= rhs.inv(); }
-    Mint operator+(const Mint& rhs) const { return Mint(*this) += rhs; }
-    Mint operator-(const Mint& rhs) const { return Mint(*this) -= rhs; }
-    Mint operator*(const Mint& rhs) const { return Mint(*this) *= rhs; }
-    Mint operator/(const Mint& rhs) const { return Mint(*this) /= rhs; }
-    Mint power(ll p) const
-    {
-        if (safe) assert(p >= 0LL || x != 0);
-        return Mint(modpow(x, p, mod));
-    }
-    bool operator==(const Mint& rhs) const { return x == rhs.x; }
-    bool operator<(const Mint& rhs) const { return x < rhs.x; }
-    friend ostream& operator<<(ostream& out, const Mint& a) { out << a.x; return out; }
-    friend istream& operator>>(istream& in, Mint& a)
-    {
-        ll x; in >> x;
-        a = Mint(x);
-        return in;
-    }
-};
 
 template<typename T, int N, int M> struct Matrix
 {
