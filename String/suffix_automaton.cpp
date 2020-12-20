9ee5d9add06e16b8ca0b8011578406cb84d41141
Célio Passos <celio.passosjr@gmail.com>
2020-12-19 22:53:39 -0300

    Remove topological sorting and add useful additional info

--- String/suffix_automaton.cpp
+++ String/suffix_automaton.cpp
@@ -18,14 +18,14 @@ struct SuffixAutomaton
 {
     struct State
     {
-        int link, len;
+        int link, len, minlen, idx;
         int nxt[K];
-        bool terminal = false;
+        bool terminal = false, clone = false;
         State(int link = -1, int len = 0) : link(link), len(len) { fill(all(nxt), -1); }
     };
     vector<State> state = { State() };
 
-    int last = 0;
+    int last = 0, total_size = 0;
     void extend(char c)
     {
         int cur = size(state), u = last, len = state[last].len + 1, ch = c - offset;
@@ -48,11 +48,16 @@ struct SuffixAutomaton
             int clone = cur + 1;
             state.emplace_back(clone, len);
             state.push_back(state[v]);
-            state[clone].len = state[u].len + 1;
-            state[v].link = clone;
+
+            state[clone].len = state[u].len + 1, state[clone].clone = true;
+            state[v].link = clone, state[v].minlen = state[clone].len + 1;
+
             for (int p = u; p != -1 && state[p].nxt[ch] == v; p = state[p].link)
                 state[p].nxt[ch] = clone;
         }
+
+        state[cur].minlen = state[state[cur].link].len + 1;
+        state[cur].idx = total_size++;
     }
 
     SuffixAutomaton(const string& s)
@@ -65,33 +70,6 @@ struct SuffixAutomaton
     int go(int u, char c) const
     {
         return state[u].nxt[c - offset];
-    }
-
-    vector<int> topological_sort() const
-    {
-        int m = size(state);
-
-        vector<int> indeg(m, 0);
-
-        for (int u = 0; u < m; ++u)
-            for (char c = offset; c < offset + K; ++c)
-                if (int v = go(u, c); v != -1) indeg[v] += 1;
-
-        static queue<int> q;
-
-        for (int u = 0; u < m; ++u) if (indeg[u] == 0) q.push(u);
-
-        vector<int> topo;
-
-        while (not empty(q))
-        {
-            int u = q.front(); q.pop();
-            topo.push_back(u);
-            for (char c = offset; c < offset + K; ++c)
-                if (int v = go(u, c); v != -1 && --indeg[v] == 0) q.push(v);
-        }
-
-        return topo;
     }
 };
 
