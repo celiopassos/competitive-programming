f8892f4ba11d2ac88c88aed82f4c9528780c8066
Matheus Resende <matheusrguedes@gmail.com>
2021-01-23 20:45:40 -0300

    fix 2sat

--- Graphs/2SAT.cpp
+++ Graphs/2SAT.cpp
@@ -8,17 +8,16 @@ struct SAT{
 	}
 	
 	void addCondition(int a, int b){
-		g[a].push_back(neg(b));
-		g[b].push_back(neg(a));
+		g[neg(a)].push_back(b);
+		g[neg(b)].push_back(a);
 	}
  
 	pair<bool, vector<int>> solve(){
 		vector<int> comp = tarjan(g), res(n);
 		for(int i = 0; i < n; i++){
 			if(comp[i]==comp[i+n])return {false, vector<int>()};
-			res[i] = comp[i] > comp[i+n];
+			res[i] = comp[i] < comp[i+n];
 		}
 		return {true, res};
 	}
 };
- 
