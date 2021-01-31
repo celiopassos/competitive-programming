f9b6344b457311aade2700ace43c08de8a2a7830
Célio Passos <celio.passosjr@gmail.com>
2021-01-31 15:37:48 -0300

    Pass by value in mint

--- Mathematics/mint.cpp
+++ Mathematics/mint.cpp
@@ -9,24 +9,24 @@ template<ll mod>
 struct Mint
 {
     ll x;
-    Mint(ll x = 0) : x((x %= mod) < 0 ? x + mod : x) {  }
-    Mint& operator+=(const Mint& rhs) { if ((x += rhs.x) >= mod) x -= mod; return *this; }
-    Mint& operator-=(const Mint& rhs) { return *this += mod - rhs.x; }
-    Mint& operator*=(const Mint& rhs) { (x *= rhs.x) %= mod; return *this; }
-    Mint& operator/=(const Mint& rhs) { return *this *= modpow(rhs.x, mod - 2, mod); }
+    Mint(ll x = 0) : x((x %= mod) < 0 ? x + mod : x) { }
+    Mint& operator+=(Mint rhs) { if ((x += rhs.x) >= mod) x -= mod; return *this; }
+    Mint& operator-=(Mint rhs) { return *this += mod - rhs.x; }
+    Mint& operator*=(Mint rhs) { (x *= rhs.x) %= mod; return *this; }
+    Mint& operator/=(Mint rhs) { return *this *= modpow(rhs.x, mod - 2, mod); }
     Mint power(ll p) const { return Mint(modpow(x, p, mod)); }
-    bool operator==(const Mint& rhs) const { return x == rhs.x; }
-    bool operator<(const Mint& rhs) const { return x < rhs.x; }
-    friend Mint operator+(const Mint& lhs, const Mint& rhs) { return Mint(lhs) += rhs; }
-    friend Mint operator-(const Mint& lhs, const Mint& rhs) { return Mint(lhs) -= rhs; }
-    friend Mint operator*(const Mint& lhs, const Mint& rhs) { return Mint(lhs) *= rhs; }
-    friend Mint operator/(const Mint& lhs, const Mint& rhs) { return Mint(lhs) /= rhs; }
-    friend ostream& operator<<(ostream& out, const Mint& a) { return out << a.x; }
+    bool operator==(Mint rhs) const { return x == rhs.x; }
+    bool operator<(Mint rhs) const { return x < rhs.x; }
+    friend Mint operator+(Mint lhs, Mint rhs) { return lhs += rhs; }
+    friend Mint operator-(Mint lhs, Mint rhs) { return lhs -= rhs; }
+    friend Mint operator*(Mint lhs, Mint rhs) { return lhs *= rhs; }
+    friend Mint operator/(Mint lhs, Mint rhs) { return lhs /= rhs; }
+    friend ostream& operator<<(ostream& out, Mint a) { return out << a.x; }
     friend istream& operator>>(istream& in, Mint& a)
     {
-        ll x; in >> x;
+        ll x;
+        in >> x;
         a = Mint(x);
         return in;
     }
 };
-
