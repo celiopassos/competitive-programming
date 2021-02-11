2c4d7451a0b54494f7b7abf384f43a7f8ce2623f
Célio Passos <celio.passosjr@gmail.com>
2021-02-11 13:52:14 -0300

    Delete hash and add things to FreeGroup

--- String/free_group.cpp
+++ String/free_group.cpp
@@ -10,6 +10,18 @@ ll modpow(ll x, ll p, ll mod)
     return res;
 }
 
+template<typename T, typename Op>
+T templatepow(T x, T Id, ll p, Op&& op)
+{
+    T res = Id;
+    while (p > 0)
+    {
+        if (p & 1) res = op(res, x);
+        p >>= 1, x = op(x, x);
+    }
+    return res;
+}
+
 template<ll mod>
 struct FreeGroup
 {
@@ -19,26 +31,31 @@ struct FreeGroup
     FreeGroup(ll shift, ll inv_shift, ll hash) : shift(shift), inv_shift(inv_shift), hash(hash) { }
 
     FreeGroup() : shift(1), inv_shift(1), hash(0) { }
-    FreeGroup(ll c) : shift(base), inv_shift(inv_base), hash(c) { }
+    FreeGroup(ll c) : shift(base), inv_shift(inv_base), hash(c + 1) { }
     FreeGroup(const string& s) : FreeGroup()
     {
-        for (auto c : s) *this = *this + FreeGroup(c);
+        for (auto c : s) *this += FreeGroup(c);
     }
 
-    FreeGroup operator+(const FreeGroup& rhs) const
+    FreeGroup& operator+=(const FreeGroup& rhs)
     {
-        ll newshift = shift * rhs.shift % mod;
-        ll newinvshit = inv_shift * rhs.inv_shift % mod;
-        ll newhash = (rhs.shift * hash + rhs.hash) % mod;
-        return FreeGroup(newshift, newinvshit, newhash);
+        shift = shift * rhs.shift % mod;
+        inv_shift = inv_shift * rhs.inv_shift % mod;
+        hash = (rhs.shift * hash + rhs.hash) % mod;
+        return *this;
     }
-    FreeGroup operator-(const FreeGroup& rhs) const
-    {
-        return *this + (-rhs);
-    }
+    FreeGroup operator-=(const FreeGroup& rhs) { *this += (-rhs); }
+    FreeGroup operator+(const FreeGroup& rhs) const { return FreeGroup(*this) += rhs; }
+    FreeGroup operator-(const FreeGroup& rhs) const { return FreeGroup(*this) -= rhs; }
     FreeGroup operator-() const
     {
         return FreeGroup(inv_shift, shift, (mod - inv_shift * hash % mod) % mod);
+    }
+    FreeGroup power(ll p) const
+    {
+        FreeGroup x = *this;
+        if (p < 0) p *= -1, x = -x;
+        return templatepow(x, FreeGroup(), p, plus<FreeGroup>());
     }
     bool operator<(const FreeGroup& rhs) const { return pair(shift, hash) < pair(rhs.shift, rhs.hash); }
     bool operator==(const FreeGroup& rhs) const { return shift == rhs.shift && hash == rhs.hash; }
