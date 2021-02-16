29bcbe90dace5dec290480ba452186734b2287c3
Célio Passos <celio.passosjr@gmail.com>
2021-02-16 13:15:02 -0300

    Better ternary search templates

--- Optimization/ternary_search.cpp
+++ Optimization/ternary_search.cpp
@@ -1,14 +1,23 @@
-template<typename T, typename... Args>
-auto ternary_search(T l, T r, T eps, auto&& f, Args... args)
+// T domain, Y image
+
+template<typename T, typename Y, typename F, typename... Args>
+pair<Y, T> ternary_search_float(T l, T r, int it, F& f, Args... args)
 {
-    while (r - l > eps)
+    for (int i = 0; i < it; ++i)
     {
         T m1 = l + (r - l) / 3, m2 = r - (r - l) / 3;
-        if (f(m1, args...) > f(m2, args...)) l = m1;
-        else r = m2;
+        f(m1, args...) > f(m2, args...) ? l = m1 : r = m2;
     }
-    T c = l;
-    for (T x = c + 1; x <= r; ++x)
-        if (f(x, args...) < f(c, args...)) c = x;
-    return pair(f(c, args...), c);
+    return pair(f(l, args...), l);
+}
+
+template<typename T, typename Y, typename F, typename... Args>
+pair<Y, T> ternary_search_int(T l, T r, F& f, Args... args)
+{
+    while (l < r)
+    {
+        T m = l + (r - l) / 2;
+        f(m, args...) > f(m + 1, args...) ? l = m + 1 : r = m;
+    }
+    return pair(f(l, args...), l);
 }
