04f9a1fb223b9328f76be9b8ade318b6da30f6f2
Célio Passos <celio.passosjr@gmail.com>
2021-03-30 21:27:33 -0300

    Add virtual tree to LCA

--- Graphs/lca_lifting.cpp
+++ Graphs/lca_lifting.cpp
@@ -2,7 +2,6 @@
 
 template<int K>
 struct LCA {
-private:
     const vector<vector<int>>& E;
     const int n;
     vector<int> L, R, h;
@@ -15,8 +14,9 @@ private:
         for (int v : E[u]) if (v != p) dfs(v, u);
         R[u] = ct++;
     }
-public:
-    LCA(const auto& E, int root) : E(E), n(size(E)), L(n), R(n), h(n), up(n) {
+
+    LCA(const auto& E, int root) : E(E), n((int)size(E)), L(n), R(n), h(n), up(n) {
+        h[root] = -1;
         dfs(root, root);
     }
     bool is_ancestor(int u, int v) const {
@@ -38,5 +38,23 @@ public:
     int distance(int u, int v) const {
         int w = query(u, v);
         return h[u] + h[v] - 2 * h[w];
+    }
+    // builds virtual tree on F and returns its root and vertex set
+    pair<int, vector<int>> build_virtual_tree(vector<int> V, vector<vector<int>>& F) {
+        auto cmp = [&](int u, int v) { return R[u] < R[v]; };
+        sort(all(V), cmp);
+        int k = (int)size(V);
+        for (int j = 0; j + 1 < k; ++j) V.push_back(query(V[j], V[j + 1]));
+        sort(all(V), cmp);
+        V.erase(unique(all(V)), end(V));
+        stack<int> stk;
+        for (auto u : V) {
+            while (not empty(stk) && is_ancestor(u, stk.top())) {
+                F[u].push_back(stk.top());
+                stk.pop();
+            }
+            stk.push(u);
+        }
+        return pair(stk.top(), V);
     }
 };
