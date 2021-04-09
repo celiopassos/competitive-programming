53efeddecb9d7eb6158d41cd61cac8e6a7f2de25
Célio Passos <celio.passosjr@gmail.com>
2021-04-09 13:12:18 -0300

    Add counting sort to SuffixAutomaton

--- String/suffix_automaton.cpp
+++ String/suffix_automaton.cpp
@@ -7,18 +7,14 @@ struct SuffixAutomaton {
         State(int link = -1, int len = 0) : link(link), len(len) { fill(all(nxt), -1); }
     };
     vector<State> st;
-
     SuffixAutomaton(const string& s) : st(1) {
         for (auto c : s) extend(c);
     }
-
     int last = 0, total_size = 0;
     void extend(char c) {
         int u = last, len = st[last].len + 1, ch = c - offset;
         last = (int)size(st);
-
         for (; u != -1 && st[u].nxt[ch] == -1; u = st[u].link) st[u].nxt[ch] = last;
-
         if (u == -1) {
             st.emplace_back(0, len);
         }
@@ -29,15 +25,21 @@ struct SuffixAutomaton {
             int clone = last + 1;
             st.emplace_back(clone, len);
             st.push_back(st[v]);
-
             st[clone].len = st[u].len + 1, st[clone].clone = true;
             st[v].link = clone, st[v].minlen = st[clone].len + 1;
-
             for (int p = u; p != -1 && st[p].nxt[ch] == v; p = st[p].link) st[p].nxt[ch] = clone;
         }
-
         st[last].minlen = st[st[last].link].len + 1;
         st[last].idx = total_size++;
     }
     int go(int u, char c) const { return st[u].nxt[c - offset]; }
+    // sorts largest to smallest
+    vector<int> topological_sort() const {
+        int m = (int)size(st);
+        vector<int> V(m), cnt(total_size + 1, 0);
+        for (int u = 0; u < m; ++u) cnt[st[u].len] += 1;
+        for (int x = total_size - 1; x >= 0; --x) cnt[x] += cnt[x + 1];
+        for (int u = 0; u < m; ++u) V[--cnt[st[u].len]] = u;
+        return V;
+    }
 };
