c551556d79daf66586fa88ffa57fd8e646ad30a5
Célio Passos <celio.passosjr@gmail.com>
2021-04-09 12:02:09 -0300

    Rework persistent segment tree

--- Data Structures/persistentst.cpp
+++ Data Structures/persistentst.cpp
@@ -10,78 +10,87 @@ class PersistentST {
 private:
     using M = Monoid;
     using T = typename Monoid::Type;
+    struct Node {
+        T value = M::Id;
+        int left = -1, right = -1, root;
+        Node (int root) : root(root) {}
+    };
     const int n;
-    vector<T> st;
-    vector<int> left, right, root, last;
-    int create(int rt) {
-        left.push_back(-1), right.push_back(-1);
-        last.push_back(rt), st.push_back(M::Id);
-        return size(st) - 1;
+    deque<Node> st;
+    int num_nodes;
+
+    int create(int rt = -1) {
+        if (rt == -1) rt = num_nodes;
+        st.emplace_back(rt);
+        return num_nodes++;
     }
-    int copy(int p, int rt) {
-        if (p != -1 && rt == last[p]) return p;
+    inline T get(int p) { return p == -1 ? M::Id : st[p].value; }
 
-        int q = create(rt);
-
-        if (p == -1) return q;
-
-        st[q] = st[p], left[q] = left[p], right[q] = right[p];
-
-        return q;
-    }
-    void modify(int p, int l, int r, int pos, T value) {
-        if (l == r) st[p] = value;
+    // p must be a reference!
+    void modify(int rt, int &p, int l, int r, int pos, T value) {
+        if (p == -1) p = create(rt);
+        else if (st[p].root != rt) {
+            int temp = p;
+            p = create();
+            st[p] = st[temp], st[p].root = rt;
+        }
+        if (l == r) st[p].value = value;
         else {
             int m = l + (r - l) / 2;
-
-            if (pos <= m) modify(left[p] = copy(left[p], last[p]), l, m, pos, value);
-
-            else modify(right[p] = copy(right[p], last[p]), m + 1, r, pos, value);
-
-            st[p] = M::op(
-                left[p] != -1 ? st[left[p]] : M::Id,
-                right[p] != -1 ? st[right[p]] : M::Id
-            );
+            pos <= m ? modify(rt, st[p].left, l, m, pos, value) : modify(rt, st[p].right, m + 1, r, pos, value);
+            st[p].value = M::op(get(st[p].left), get(st[p].right));
         }
     }
     T query(int p, int l, int r, int ql, int qr) {
         if (p == -1 || r < ql || qr < l) return M::Id;
-
-        if (ql <= l && r <= qr) return st[p];
-
+        if (ql <= l && r <= qr) return st[p].value;
         int m = l + (r - l) / 2;
-
-        T resl = query(left[p], l, m, ql, qr);
-        T resr = query(right[p], m + 1, r, ql, qr);
-
+        T resl = query(st[p].left, l, m, ql, qr);
+        T resr = query(st[p].right, m + 1, r, ql, qr);
         return M::op(resl, resr);
     }
-    void build(int p, int l, int r, const vector<T>& a) {
-        if (l == r) st[p] = a[l];
+    int merge(int rt, int p, int q) {
+        if (p == -1) return q;
+        if (q == -1) return p;
+        int r = create(rt);
+        st[r].value = M::op(get(p), get(q));
+        st[r].left = merge(rt, st[p].left, st[q].left);
+        st[r].right = merge(rt, st[p].right, st[q].right);
+        return r;
+    }
+    void build(int rt, int p, int l, int r, const vector<T>& a) {
+        if (l == r) st[p].value = a[l];
         else {
             int m = l + (r - l) / 2;
-
-            left[p] = create(0), right[p] = create(0);
-
-            build(left[p], l, m, a), build(right[p], m + 1, r, a);
-
-            st[p] = M::op(st[left[p]], st[right[p]]);
+            st[p].left = create(rt), st[p].right = create(rt);
+            build(rt, st[p].left, l, m, a), build(rt, st[p].right, m + 1, r, a);
+            st[p].value = M::op(get(st[p].left), get(st[p].right));
         }
     }
 public:
-    PersistentST(int n) : n(n), root(1, create(0)) { }
-    PersistentST(const vector<T>& a) : PersistentST(size(a)) {
-        build(0, 0, n - 1, a);
+    PersistentST(int n) : n(n), num_nodes(0) { create(); }
+    int build(const vector<T>& a) {
+        assert(size(a) == n);
+        int rt = create();
+        build(rt, rt, 0, n - 1, a);
+        return rt;
     }
-    int duplicate(int version) {
-        root.push_back(copy(root[version], size(root)));
-        return size(root) - 1;
+    int duplicate(int root) {
+        int rt = create();
+        st[rt] = st[root];
+        st[rt].root = rt;
+        return rt;
     }
-    void modify(int version, int pos, T value) // modifies version in place {
-        assert(version < size(root));
-        modify(root[version], 0, n - 1, pos, value);
+    // doesn't make sense with uncommutative operations, versions are not modified
+    // O(size of tree overlap)
+    int merge(int root1, int root2) {
+        return merge(num_nodes, root1, root2);
     }
-    T query(int version, int l, int r) {
-        return query(root[version], 0, n - 1, l, r);
+    // modifies version in place
+    void modify(int root, int pos, T value) {
+        modify(root, root, 0, n - 1, pos, value);
+    }
+    T query(int root, int l, int r) {
+        return query(root, 0, n - 1, l, r);
     }
 };
