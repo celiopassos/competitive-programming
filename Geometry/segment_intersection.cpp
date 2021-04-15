e1341535b97cda6b8064f79c4af0baddd307a06f
Célio Passos <celio.passosjr@gmail.com>
2021-04-15 14:04:43 -0300

    Fix segment intersection

--- Geometry/segment_intersection.cpp
+++ Geometry/segment_intersection.cpp
@@ -9,16 +9,16 @@ template<typename T>
 T sgn(T x) { return x > 0 ? +1 : x == 0 ? 0 : -1; }
 
 template<typename T>
-bool intersects(pair<complex<T>, complex<T>> U, pair<complex<T>, complex<T>> V) {
+bool intersects(array<complex<T>, 2> U, array<complex<T>, 2> V) {
     auto [a, b] = U;
     auto [c, d] = V;
 
     // checks if segments are on the same line
     // return true if Ox and Oy projections intersect
     if (cross(a - c, d - c) == 0 && cross(b - c, d - c) == 0)
-        return intersect(a.x(), b.x(), c.x(), d.x()) && intersect(a.y(), b.y(), c.y(), d.y());
+        return intersects(a.x(), b.x(), c.x(), d.x()) && intersects(a.y(), b.y(), c.y(), d.y());
 
-    // checks if a and b line on the same side of the segment c->d, and vice-versa
+    // checks if a and b don't lie on the same side of the segment c->d, and vice-versa
     return sgn(cross(b - a, c - a)) != sgn(cross(b - a, d - a)) &&
            sgn(cross(d - c, a - c)) != sgn(cross(d - c, b - c));
 }
