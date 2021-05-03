6e91ce9af66578eee84c3d6733b618bcbf4e6cec
Célio Passos <celio.passosjr@gmail.com>
2021-05-03 09:02:51 -0300

    Add identity FreeGroup warning

--- String/free_group.cpp
+++ String/free_group.cpp
@@ -1,7 +1,6 @@
 const int FIXED_RANDOM = (int)chrono::steady_clock::now().time_since_epoch().count();
 mt19937 rng(FIXED_RANDOM);
-
-uniform_int_distribution<ll> unif(1e5, 1e6);
+uniform_int_distribution<ll> unif;
 
 ll modpow(ll x, ll p, ll mod) {
     ll res = 1;
@@ -9,19 +8,12 @@ ll modpow(ll x, ll p, ll mod) {
     return res;
 }
 
-template<typename T, typename Op>
-T templatepow(T x, T Id, ll p, Op&& op) {
-    T res = Id;
-    while (p > 0) {
-        if (p & 1) res = op(res, x);
-        p >>= 1, x = op(x, x);
-    }
-    return res;
-}
+// Beware that FreeGroup() is the identity, NOT FreeGroup(0)!
+// Not commutative (of course), so pay attention when querying substrings via prefix sums.
 
 template<ll mod>
 struct FreeGroup {
-    inline static const ll base = unif(rng), inv_base = modpow(base, mod - 2, mod);
+    inline static const ll base = max<ll>(1e6, unif(rng) % mod), inv_base = modpow(base, mod - 2, mod);
 
     ll shift, inv_shift, hash;
     FreeGroup(ll shift, ll inv_shift, ll hash) : shift(shift), inv_shift(inv_shift), hash(hash) { }
@@ -38,7 +30,7 @@ struct FreeGroup {
         hash = (rhs.shift * hash + rhs.hash) % mod;
         return *this;
     }
-    FreeGroup operator-=(const FreeGroup& rhs) { *this += (-rhs); }
+    FreeGroup operator-=(const FreeGroup& rhs) { return *this += (-rhs); }
     FreeGroup operator+(const FreeGroup& rhs) const { return FreeGroup(*this) += rhs; }
     FreeGroup operator-(const FreeGroup& rhs) const { return FreeGroup(*this) -= rhs; }
     FreeGroup operator-() const {
@@ -47,9 +39,14 @@ struct FreeGroup {
     FreeGroup power(ll p) const {
         FreeGroup x = *this;
         if (p < 0) p *= -1, x = -x;
-        return templatepow(x, FreeGroup(), p, plus<FreeGroup>());
+        FreeGroup res;
+        while (p) {
+            if (p & 1) res = res + x;
+            x += x, p >>= 1;
+        }
+        return res;
     }
     bool operator<(const FreeGroup& rhs) const { return pair(shift, hash) < pair(rhs.shift, rhs.hash); }
     bool operator==(const FreeGroup& rhs) const { return shift == rhs.shift && hash == rhs.hash; }
-    bool operator!=(const FreeGroup& rhs) const { return not *this == rhs; }
+    bool operator!=(const FreeGroup& rhs) const { return not (*this == rhs); }
 };
