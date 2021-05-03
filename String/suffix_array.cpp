a8f50f88a40e29631b22f225b6c62dcc04a2c9d9
Célio Passos <celio.passosjr@gmail.com>
2021-05-03 19:22:33 -0300

    Add suffix tree from suffix array

--- String/suffix_array.cpp
+++ String/suffix_array.cpp
@@ -1,5 +1,4 @@
-template<typename ArrayType>
-vector<int> sort_cyclic_shifts(const ArrayType& s) {
+vector<int> sort_cyclic_shifts(const string& s) {
     int n = (int)size(s);
     vector<int> p(n), inv(n), tmp(n), cnt(n);
     iota(all(p), 0);
@@ -26,38 +25,71 @@ vector<int> sort_cyclic_shifts(const ArrayType& s) {
     return p;
 }
 
-vector<int> suffix_array(const string& s) {
-    auto p = sort_cyclic_shift(s + char(0));
-    p.erase(begin(p));
-    return p;
-}
-
-template<typename T>
-vector<int> suffix_array(vector<T> s) {
-    s.push_back(numeric_limits<T>::min());
-    auto p = sort_cyclic_shift(s);
-    p.erase(begin(p));
-    return p;
-}
-
-template<typename S>
-vector<int> lcp_array(const S& s, const vector<int>& p, const vector<int>& pos) {
-    int n = (int)size(s);
-    vector<int> lcp(n, 0);
-    for (int i = 0, k = 0; i < n; ++i, k = max(0, k - 1)) {
-        if (pos[i] == n - 1) {
-            k = 0;
-            continue;
-        }
-        for (int j = p[pos[i] + 1]; max(i, j) + k < n && s[i + k] == s[j + k]; ++k);
-        lcp[pos[i]] = k;
+struct SuffixArray {
+    const string s;
+    const int n;
+    vector<int> p, pos, lcp;
+    SuffixArray(const string& s_) : s(s_ + char(0)), n((int)size(s)), pos(n, 0) {
+        p = sort_cyclic_shifts(s);
+        for (int i = 0; i < n; ++i) pos[p[i]] = i;
     }
-    return lcp;
-}
-
-template<typename RMQ>
-int lcp_query(int i, int j, const vector<int>& pos, const RMQ& rmq) {
-    if (i == j) return (int)size(pos) - i;
-    if (pos[i] > pos[j]) swap(i, j);
-    return rmq.query(pos[i] + 1, pos[j]);
-}
+    // lcp[0] = 0, lcp[i] = longest common prefix of p[i - 1] and p[i] for i > 0
+    void build_lcp() {
+        lcp.assign(n - 1, 0);
+        for (int i = 0, k = 0; i < n - 1; ++i, k = max(0, k - 1)) {
+            if (pos[i] == n - 1) {
+                k = 0;
+                continue;
+            }
+            for (int j = p[pos[i] + 1]; max(i, j) + k < n && s[i + k] == s[j + k]; ++k);
+            lcp[pos[i]] = k;
+        }
+    }
+    template<typename RMQ> int lcp_query(int i, int j, const RMQ& rmq) const {
+        if (i == j) return (int)size(pos) - i;
+        if (pos[i] > pos[j]) swap(i, j);
+        return rmq.query(pos[i], pos[j] - 1);
+    }
+    struct Node {
+        int link, len, idx;
+    };
+    vector<Node> st;
+    int create(int len, int idx) {
+        st.push_back({-1 , len, 0, idx});
+        return (int)size(st) - 1;
+    }
+    void set_link(int u, int p) {
+        st[u].link = p, st[u].minlen = st[p].len + 1;
+    }
+    void build_suffix_tree() {
+        stack<int> stk, children;
+        stk.push(create(0, -1));
+        for (int i = 1; i < n - 1; ++i) {
+            int sufflen = n - 1 - p[i];
+            if (lcp[i] < sufflen) {
+                int u = create(sufflen, p[i]);
+                if (lcp[i] <= lcp[i - 1]) set_link(u, stk.top());
+                else children.push(u);
+            }
+            while (st[stk.top()].len > lcp[i]) {
+                int v = stk.top();
+                stk.pop();
+                if (st[stk.top()].len >= lcp[i]) set_link(v, stk.top());
+                else children.push(v);
+            }
+            if (lcp[i] > st[stk.top()].len) {
+                stk.push(create(lcp[i], p[i]));
+                while (not empty(children)) {
+                    set_link(children.top(), stk.top());
+                    children.pop();
+                }
+            }
+        }
+        stk.push(create(n - 1 - p.back(), p.back()));
+        while (size(stk) > 1) {
+            int u = stk.top();
+            stk.pop();
+            set_link(u, stk.top());
+        }
+    }
+};
