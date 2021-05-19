4f1590d66896d461019e8adfd74a1c9849af0fd7
Resende <matheusrguedes@gmail.com>
2021-05-18 21:28:28 -0300

    fix centroid

--- Graphs/centroid.cpp
+++ Graphs/centroid.cpp
@@ -11,30 +11,23 @@ private:
 
     int dfs(int u, int p, int h) {
         if (h != -1) dist[u][h] = dist[p][h] + 1;
-
         weight[u] = 1;
-
         for (auto v : E[u]) if (not vis[v] && v != p)
             weight[u] += dfs(v, u, h);
-
         return weight[u];
     }
     int find_centroid(int u, int p, int cut) {
         for (auto v : E[u])
             if (not vis[v] && v != p && weight[v] > cut)
                 return find_centroid(v, u, cut);
-
         return u;
     }
     void build(int u, int p) {
         int total = dfs(u, p, p == -1 ? -1 : level[p]);
         int centroid = find_centroid(u, p, total / 2);
-
         if (p != -1) level[centroid] = level[p] + 1;
-
         parent[centroid] = p, vis[centroid] = 1;
         dist[centroid][level[centroid]] = 0;
-
         for (auto v : E[centroid]) if (not vis[v]) build(v, centroid);
     }
 public:
@@ -48,18 +41,14 @@ public:
     int getlevel(int u) const {
         return level[u];
     }
-    int lca(int u, int v) const // centroid lca, not tree lca {
+    int lca(int u, int v) const{ // centroid lca, not tree lca
         if (level[u] < level[v]) swap(u, v);
-
         while (level[u] > level[v]) u = parent[u];
-
         while (u != v) u = parent[u], v = parent[v];
-
         return u;
     }
     int distance(int u, int v) const {
         int w = lca(u, v);
-
         return dist[u][level[w]] + dist[v][level[w]];
     }
 };
