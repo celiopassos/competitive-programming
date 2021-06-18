974cda65be9719bf493de438fca099559455160b
Célio Passos <celio.passosjr@gmail.com>
2021-06-17 22:42:37 -0300

    Separate stuff in basic.cpp

--- Geometry/complex.cpp
+++ Geometry/complex.cpp
@@ -1,11 +1,6 @@
 #define x real
 #define y imag
 template<typename T>
-using Segment = array<complex<T>, 2>;
-// represents points p with dot(Line.first, p) = Line.second
-template<typename T>
-using Line = pair<complex<T>, T>;
-template<typename T>
 T dot(complex<T> a, complex<T> b) { return (conj(a) * b).x(); }
 template<typename T>
 T cross(complex<T> a, complex<T> b) { return (conj(a) * b).y(); }
@@ -13,8 +8,6 @@ template<typename T>
 bool cw(complex<T> a, complex<T> b, complex<T> c) { return cross(b - a, c - b) < 0; }
 template<typename T>
 bool ccw(complex<T> a, complex<T> b, complex<T> c) { return cross(b - a, c - b) > 0; }
-template<typename T>
-complex<T> normalized(complex<T> a) { return a / gcd<T>(a.x(), a.y()); }
 template<typename T>
 istream& operator>>(istream& in, complex<T>& p) {
     T x, y;
---
+++ Geometry/line.cpp
@@ -0,0 +1,4 @@
+template<typename T>
+using Line = pair<complex<T>, T>; // hyperplane dot(first, x) = second
+template<typename T>
+using Segment = array<complex<T>, 2>;
