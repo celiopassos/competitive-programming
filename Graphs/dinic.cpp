ad45da930eff52400e78abd2ba9aab783130ce05
Célio Passos <celio.passosjr@gmail.com>
2021-06-24 18:39:56 -0300

    Remove references from dinic

--- Graphs/dinic.cpp
+++ Graphs/dinic.cpp
@@ -4,29 +4,30 @@ struct Dinic {
         int from, to;
         T cap, flow = 0;
         T free() { return cap - flow; }
-        Edge(int u, int v, const T& cap) : from(u), to(v), cap(cap) {}
+        Edge(int u, int v, T cap) : from(u), to(v), cap(cap) {}
     };
     vector<Edge> edges;
-    const T inf = numeric_limits<T>::max(), zero = 0;
+    const T inf = numeric_limits<T>::max();
     int n, m = 0;
     vector<vector<int>> E;
     vector<int> level, ptr;
-    Dinic(int n) : n(n) {
-        E.resize(n), level.resize(n), ptr.resize(n);
-    }
-    int add_edge(int u, int v, const T& cap) {
+    Dinic(int n) : n(n), E(n), level(n), ptr(n) {}
+    int add_edge(int u, int v, T cap) {
         assert(cap >= 0);
         edges.emplace_back(u, v, cap);
-        edges.emplace_back(v, u, zero);
+        edges.emplace_back(v, u, 0);
         E[u].push_back(m++);
         E[v].push_back(m++);
         return m - 2;
     }
     bool bfs(int s, int t) {
-        fill(begin(level), end(level), 0); level[s] = 1;
-        static queue<int> q; q.push(s);
+        fill(begin(level), end(level), 0);
+        level[s] = 1;
+        static queue<int> q;
+        q.push(s);
         while (not q.empty()) {
-            int u = q.front(); q.pop();
+            int u = q.front();
+            q.pop();
             for (auto idx : E[u]) {
                 int v = edges[idx].to;
                 if (level[v] != 0 || edges[idx].free() <= 0) continue;
