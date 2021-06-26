8b5642079ddd71949bc5f18d5fa1abeb35b0ddc2
Célio Passos <celio.passosjr@gmail.com>
2021-06-26 15:31:12 -0300

    OffsetClamp constructor

--- Other/clamp.cpp
+++ Other/clamp.cpp
@@ -5,21 +5,26 @@ struct Clamp {
     T low, high;
     Clamp(T low = linf, T high = rinf) : low(low), high(high) {}
     // composition, rhs is applied first, this is applied after
-    Clamp operator+(Clamp rhs) const {
+    Clamp operator*(Clamp rhs) const {
         if (high <= rhs.low) return Clamp(high, high);
         if (low >= rhs.high) return Clamp(low, low);
         return Clamp(max(low, rhs.low), min(high, rhs.high));
     }
-    T operator()(T x) const { return std::clamp(x, low, high); }
+    T operator()(T x) const {
+        return std::clamp(x, low, high);
+    }
 };
 template<typename T>
 struct OffsetClamp {
     Clamp<T> cl;
     T add;
+    OffsetClamp(T low = Clamp<T>::linf, T high = Clamp<T>::rinf, T add = 0) : cl(low, high), add(add) {}
     OffsetClamp(Clamp<T> cl = Clamp<T>(), T add = 0) : cl(cl), add(add) {}
-    OffsetClamp operator+(OffsetClamp rhs) const {
+    OffsetClamp operator*(OffsetClamp rhs) const {
         Clamp<T> inside_cl(rhs.cl.low + add, rhs.cl.high + add);
-        return OffsetClamp(cl + inside_cl, add + rhs.add);
+        return OffsetClamp(cl * inside_cl, add + rhs.add);
     }
-    T operator()(T x) { return cl(x + add); }
+    T operator()(T x) const {
+        return cl(x + add);
+    }
 };
