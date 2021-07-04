401c697f2758d8abb38b465fbae891f776d54d2d
Célio Passos <celio.passosjr@gmail.com>
2021-07-04 01:28:10 -0300

    Switch roles of L and R in binary lifting

--- Graphs/lca_lifting.cpp
+++ Graphs/lca_lifting.cpp
@@ -3,17 +3,20 @@ template<int K>
 struct LCA {
     const vector<vector<int>>& E;
     int n;
-    vector<int> L, R;
+    vector<int> L, R, inv, h;
     vector<array<int, K>> up;
     int timer = 0;
     void dfs(int u, int p) {
         up[u][0] = p;
         for (int i = 1; i < K; ++i) up[u][i] = up[up[u][i-1]][i-1];
-        L[u] = timer;
-        for (int v : E[u]) if (v != p) dfs(v, u);
-        R[u] = timer++;
+        inv[L[u] = timer++] = u;
+        for (int v : E[u]) if (v != p) {
+            h[v] = h[u] + 1;
+            dfs(v, u);
+        }
+        R[u] = timer - 1;
     }
-    LCA(const auto& E, int root) : E(E), n((int)size(E)), L(n), R(n), up(n) {
+    LCA(const auto& E, int root) : E(E), n((int)size(E)), L(n), R(n), inv(n), h(n), up(n) {
         dfs(root, root);
     }
     bool is_ancestor(int u, int v) const {
@@ -26,5 +29,9 @@ struct LCA {
             if (!is_ancestor(up[u][i], v)) u = up[u][i];
         }
         return up[u][0];
+    }
+    int dist(int u, int v) {
+        int w = lca(u, v);
+        return h[u] + h[v] - 2 * h[w];
     }
 };
