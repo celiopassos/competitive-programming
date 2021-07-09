012000c3ff9aec1a490f23ea9ab77cca63876b39
Célio Passos <celio.passosjr@gmail.com>
2021-07-08 21:51:53 -0300

    Fix virtual tree

--- Graphs/lca_stuff.cpp
+++ Graphs/lca_stuff.cpp
@@ -7,17 +7,17 @@ int query(int u, int v, int root, const LCA& lca) {
     else return w[0];
 }
 // builds virtual tree on F given the vertex set V and returns its root
-// V is modified to contain the new vertices and is sorted by R[u]
+// V is modified to contain the new vertices and is sorted by L[u]
 template<typename LCA>
 int build_virtual_tree(vector<int>& V, vector<vector<int>>& F, const LCA& lca) {
-    auto cmp = [&](int u, int v) { return lca.R[u] < lca.R[v]; };
+    auto cmp = [&](int u, int v) { return lca.L[u] > lca.L[v]; };
     sort(begin(V), end(V), cmp);
     int k = (int)size(V);
     for (int j = 0; j + 1 < k; ++j) V.push_back(lca.lca(V[j], V[j + 1]));
     sort(begin(V), end(V), cmp), V.erase(unique(begin(V), end(V)), end(V));
     stack<int> stk;
     for (auto u : V) {
-        while (not empty(stk) && is_ancestor(u, stk.top())) {
+        while (not empty(stk) && lca.is_ancestor(u, stk.top())) {
             F[u].push_back(stk.top());
             stk.pop();
         }
