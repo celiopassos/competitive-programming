ec14fbec8b99045a9aa3d6d1cf74c2b4997328c1
Célio Passos <celio.passosjr@gmail.com>
2021-07-11 10:43:21 -0300

    Simpler bit reversal in FFT

--- Mathematics/fft.cpp
+++ Mathematics/fft.cpp
@@ -9,28 +9,25 @@ complex<T> getroot(int N) {
 // inplace fft (resizes if necessary)
 template<typename T>
 void fft(vector<T>& p, bool inverse) {
-    int N = 1, logN = 0;
-    while ((int)size(p) > N) N <<= 1, ++logN;
-    p.resize(N);
-    for (int i = 0; i < N; ++i) {
-        int rev = 0;
-        for (int k = 0; k < logN; ++k) {
-            rev |= (i >> k & 1) << (logN - 1 - k);
-        }
-        if (i < rev) swap(p[i], p[rev]);
-    }
-    vector<T> q(N);
-    for (int k = 1; k <= logN; ++k) {
-        int len = 1 << k, hlen = len / 2;
-        T rt = getroot(len);
-        if (inverse) rt = 1 / rt;
+    int N = 1;
+    while ((int)size(p) > N) N *= 2;
+    static vector<T> q;
+    p.resize(N), q.resize(N);
+    for (int len = N; len > 1; len /= 2, swap(p, q)) {
         for (int l = 0; l < N; l += len) {
-            T x = 1;
-            for (int i = 0; i < len; ++i, x *= rt) {
+            int c[2] = {l, l + len / 2};
+            for (int i = 0; i < len; ++i) {
+                q[c[i % 2] + i / 2] = p[l + i];
+            }
+        }
+    }
+    for (int len = 2, hlen = 1; len <= N; len *= 2, hlen *= 2, swap(p, q)) {
+        T rt = inverse ? getroot(len) : 1 / getroot(len);
+        for (int l = 0; l < N; l += len) {
+            for (auto [i, x] = pair(0, T(1)); i < len; ++i, x *= rt) {
                 q[l + i] = p[l + i % hlen] + x * p[l + hlen + i % hlen];
             }
         }
-        swap(p, q);
     }
     if (inverse) {
         T inv = 1 / T(N);
