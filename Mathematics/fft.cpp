289d995eaa88393e128863b8b1355981b372b30e
Célio Passos <celio.passosjr@gmail.com>
2021-07-16 21:28:31 -0300

    Remove one loop from fft

--- Mathematics/fft.cpp
+++ Mathematics/fft.cpp
@@ -11,22 +11,20 @@ template<typename T>
 void fft(vector<T>& p, bool inverse) {
     int N = 1;
     while ((int)size(p) > N) N *= 2;
-    static vector<T> q;
-    p.resize(N), q.resize(N);
-    for (int len = N; len > 1; len /= 2, swap(p, q)) {
-        for (int l = 0; l < N; l += len) {
-            int c[2] = {l, l + len / 2};
-            for (int i = 0; i < len; ++i) {
-                q[c[i % 2] + i / 2] = p[l + i];
-            }
+    p.resize(N);
+    vector<T> q(N);
+    for (int i = 0; i < N; ++i) {
+        int rev = 0;
+        for (int b = 1; b < N; b <<= 1) {
+            rev = (rev << 1) | !!(i & b);
         }
+        q[rev] = p[i];
     }
-    for (int len = 2, hlen = 1; len <= N; len *= 2, hlen *= 2, swap(p, q)) {
-        T rt = inverse ? getroot(len) : 1 / getroot(len);
-        for (int l = 0; l < N; l += len) {
-            for (auto [i, x] = pair(0, T(1)); i < len; ++i, x *= rt) {
-                q[l + i] = p[l + i % hlen] + x * p[l + hlen + i % hlen];
-            }
+    swap(p, q);
+    for (int b = 1; b < N; b <<= 1, swap(p, q)) {
+        T rt = inverse ? getroot(b << 1) : 1 / getroot(b << 1);
+        for (auto [i, x] = pair(0, T(1)); i < N; ++i, x *= rt) {
+            q[i] = p[i & ~b] + x * p[i | b];
         }
     }
     if (inverse) {
