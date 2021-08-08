fd594beb941ab13aba2b7cdff7b143a457e27654
Célio Passos <celio.passosjr@gmail.com>
2021-08-08 10:31:21 -0300

    Fix dijkstra again

--- Graphs/dijkstra.cpp
+++ Graphs/dijkstra.cpp
@@ -1,18 +1,18 @@
 template<typename T>
 vector<T> dijkstra(int s, const vector<vector<pair<int, T>>>& E) {
-    vector<T> d(E.size(), numeric_limits<T>::max());
+    vector<T> dist(E.size(), numeric_limits<T>::max());
     priority_queue<pair<T, int>> pq;
-    pq.emplace(d[s] = 0, s);
+    pq.emplace(dist[s] = 0, s);
     while (not pq.empty()) {
         auto [d, u] = pq.top();
         pq.pop();
-        if (-d > d[u]) continue;
+        if (-d > dist[u]) continue;
         for (auto [v, w] : E[u]) {
-            T nd = d[u] + w;
-            if (nd < d[v]) {
-                pq.emplace(-(d[v] = nd), v);
+            T nd = dist[u] + w;
+            if (nd < dist[v]) {
+                pq.emplace(-(dist[v] = nd), v);
             }
         }
     }
-    return d;
+    return dist;
 }
