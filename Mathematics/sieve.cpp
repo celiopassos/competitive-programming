99c740a1206b0e2247ae1416580d68b6cd66c386
Célio <celio.passosjr@gmail.com>
2021-11-11 12:31:52 -0300

    Reindent

--- Mathematics/sieve.cpp
+++ Mathematics/sieve.cpp
@@ -1,7 +1,9 @@
 std::pair<std::vector<int>, std::vector<int>> sieve(int N) {
-    std::vector<int> lp(N + 1, 0), pr;
+    std::vector<int> lp(N + 1), pr;
     for (int i = 2; i <= N; ++i) {
-        if (lp[i] == 0) pr.push_back(lp[i] = i);
+        if (lp[i] == 0) {
+            pr.push_back(lp[i] = i);
+        }
         for (auto p : pr) {
             if (p > lp[i] || i * p > N) break;
             lp[i * p] = p;
