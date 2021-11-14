9d028f969ad6e7b9b7728e100224a50e41472bb0
Célio <celio.passosjr@gmail.com>
2021-11-13 21:19:56 -0300

    Add SuffixArray struct

--- String/suffix_array.cpp
+++ String/suffix_array.cpp
@@ -25,3 +25,33 @@ std::vector<int> sort_cyclic_shifts(Iterator first, Iterator last) {
     }
     return p;
 }
+struct SuffixArray {
+    int N;
+    std::vector<int> p, pos, lcp;
+    SuffixArray(std::string S) {
+        S.push_back(0);
+        N = S.size();
+        p = sort_cyclic_shifts(S.begin(), S.end());
+        pos.resize(N);
+        lcp.resize(N);
+        for (int i = 0; i < N; ++i) {
+            pos[p[i]] = i;
+        }
+        for (int i = 0, k = 0; i < N - 1; ++i, k = std::max(0, k - 1)) {
+            if (pos[i] == N - 1) {
+                k = 0;
+                continue;
+            }
+            for (int j = p[pos[i] + 1]; std::max(i, j) + k < N && S[i + k] == S[j + k]; ++k);
+            lcp[pos[i]] = k;
+        }
+    }
+    template<typename RMQ>
+    int lcp_query(int i, int j, const RMQ& rmq) const {
+        if (i == j) return N - i - 1;
+        if (pos[i] > pos[j]) {
+            std::swap(i, j);
+        }
+        return rmq.query(pos[i], pos[j]); // range query should be [l, r)
+    }
+}
