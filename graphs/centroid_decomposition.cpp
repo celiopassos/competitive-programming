39c5bade7d5d2b6314575d0449994096e6acfc43
Célio <celio.passosjr@gmail.com>
2022-01-28 23:33:27 -0300

    Better centroid decomposition

--- graphs/centroid_decomposition.cpp
+++ graphs/centroid_decomposition.cpp
@@ -1,68 +1,76 @@
-// K > centroid tree height, or simply log(N)
-template <int K>
 struct CentroidDecomposition {
+  struct Info {
+    int root, dist, bucket;
+    Info(int root, int dist, int bucket) :
+      root(root), dist(dist), bucket(bucket) {}
+  };
   const std::vector<std::vector<int>>& E;
-  int N, root;
-  std::vector<int> sz, p, subtree, dead, level;
-  std::vector<std::array<int, K>> dist;
-  int timer;
-  void dfs(int u, int p, int k) {
-    sz[u] = 1;
+  int N, num_buckets = 0;
+  std::vector<bool> dead;
+  std::vector<int> size, level;
+  std::vector<std::vector<Info>> info;
+  int find_centroid(int u, int p, int n) const {
     for (auto v : E[u]) {
       if (v == p || dead[v]) continue;
-      dist[v][k] = dist[u][k] + 1;
-      dfs(v, u, k);
-      sz[u] += sz[v];
-    }
-  }
-  int find_centroid(int u, int p, int need) const {
-    for (auto v : E[u]) {
-      if (v == p || dead[v]) continue;
-      if (sz[v] > need) return find_centroid(v, u, need);
+      if (size[v] > n / 2) {
+        return find_centroid(v, u, n);
+      }
     }
     return u;
   }
-  CentroidDecomposition(const std::vector<std::vector<int>>& E_)
-      : E(E_),
-        N(E.size()),
-        sz(N),
-        p(N, -1),
-        subtree(N),
-        dead(N),
-        level(N),
-        dist(N) {
+  void dfs(int u, int p) {
+    size[u] = 1;
+    for (auto v : E[u]) {
+      if (v == p || dead[v]) continue;
+      info[v].push_back(info[u].back());
+      ++info[v].back().dist;
+      dfs(v, u);
+      size[u] += size[v];
+    }
+  }
+  CentroidDecomposition(const std::vector<std::vector<int>>& E) :
+    E(E), N(E.size()), dead(N), size(N), level(N), info(N)
+  {
     std::queue<int> q;
-    dfs(0, -1, 0);
-    root = find_centroid(0, -1, N / 2);
-    q.push(root);
-    dist[root][0] = 0;
+    q.push(0);
     while (!q.empty()) {
       int rt = q.front();
       q.pop();
       dead[rt] = true;
-      int k = level[rt];
-      dfs(rt, -1, level[rt]);
       for (auto v : E[rt]) {
         if (dead[v]) continue;
-        int u = find_centroid(v, -1, sz[v] / 2);
-        p[u] = rt;
-        level[u] = k + 1;
-        subtree[u] = v;
-        q.push(u);
+        info[v].emplace_back(rt, 1, num_buckets++);
+        dfs(v, rt);
+        int nrt = find_centroid(v, rt, size[v]);
+        level[nrt] = level[rt] + 1;
+        q.push(nrt);
       }
     }
   }
-  int operator[](int u) const {
-    return p[u];
+  int parent(int u) const {
+    return info[u].empty() ? -1 : info[u].back().root;
   }
   int lca(int u, int v) const {
-    if (level[u] < level[v]) std::swap(u, v);
-    while (level[u] > level[v]) u = p[u];
-    while (u != v) u = p[u], v = p[v];
+    while (level[u] > level[v]) {
+      u = parent(u);
+    }
+    while (level[v] > level[u]) {
+      v = parent(v);
+    }
+    while (u != v) {
+      u = parent(u);
+      v = parent(v);
+    }
     return u;
   }
-  int distance(int u, int v) const {
+  int dist(int u, int v) const {
     int w = lca(u, v);
-    return dist[u][level[w]] + dist[v][level[w]];
+    int res = 0;
+    for (auto x : {u, v}) {
+      if (level[w] < level[x]) {
+        res += info[x][level[w]].dist;
+      }
+    }
+    return res;
   }
 };
