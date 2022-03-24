71f7a2513c63f97649cf5d3af47e93a3ef82f112
Célio Passos <celio.passosjr@gmail.com>
2022-03-24 19:29:14 -0300

    Better sieve

--- mathematics/sieve.cpp
+++ mathematics/sieve.cpp
@@ -1,13 +1,31 @@
-std::pair<std::vector<int>, std::vector<int>> sieve(int N) {
-  std::vector<int> lp(N + 1), pr;
-  for (int i = 2; i <= N; ++i) {
-    if (lp[i] == 0) {
-      pr.push_back(lp[i] = i);
-    }
-    for (auto p : pr) {
-      if (p > lp[i] || i * p > N) break;
-      lp[i * p] = p;
+struct Sieve {
+  struct PrimePower {
+    int p = -1, pow, k;  // pow = p^k
+  };
+  std::vector<PrimePower> lp;
+  std::vector<int> primes;
+  Sieve(int N) : lp(N + 1) {
+    for (int i = 2; i <= N; ++i) {
+      if (lp[i].p == -1) {
+        lp[i].p = lp[i].pow = i;
+        lp[i].k = 1;
+        primes.push_back(i);
+      }
+      for (auto p : primes) {
+        if (i * p > N || p > lp[i].p) break;
+        int j = i * p;
+        lp[j].p = p;
+        if (p == lp[i].p) {
+          lp[j].pow = p * lp[i].pow;
+          lp[j].k = lp[i].k + 1;
+        } else {
+          lp[j].pow = p;
+          lp[j].k = 1;
+        }
+      }
     }
   }
-  return {lp, pr};
-}
+  const PrimePower& operator[](int i) const {
+    return lp[i];
+  }
+};
